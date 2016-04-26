28a6b762431009e860f33632d62278da2fe010df
atrav <travelling.with.code@gmail.com>
2016-04-26 16:06:27 +0300

    Docker-compose.yml added.

--- src/main/java/travelling/with/code/open/flashcards/MongoConfiguration.java
+++ src/main/java/travelling/with/code/open/flashcards/MongoConfiguration.java
@@ -1,7 +1,5 @@
 package travelling.with.code.open.flashcards;
 
-import java.util.Map;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -12,7 +10,7 @@ import com.mongodb.MongoClient;
 @Configuration
 public class MongoConfiguration extends AbstractMongoConfiguration {
 
-    private static final String MONGO_ADDR = "MONGO_PORT_27017_TCP_ADDR";
+//    private static final String MONGO_ADDR = "MONGO_PORT_27017_TCP_ADDR";
     private static final String DB_NAME = "flashcards";
 
     @Override
@@ -23,8 +21,9 @@ public class MongoConfiguration extends AbstractMongoConfiguration {
     @Override
     @Bean
     public Mongo mongo() throws Exception {
-        Map<String, String> enviromentalVariables = System.getenv();
-        String mongoIp = enviromentalVariables.get(MONGO_ADDR);
-        return new MongoClient(mongoIp != null ? mongoIp : "localhost", 27017);
+//        Map<String, String> enviromentalVariables = System.getenv();
+//        String mongoIp = enviromentalVariables.get(MONGO_ADDR);
+//        return new MongoClient(mongoIp != null ? mongoIp : "localhost", 27017);
+        return new MongoClient("mongo", 27017);
     }
 }
